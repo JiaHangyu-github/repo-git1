0a96c57284f75752f94a94bc4c09812782e00dbd
贾航宇 <1092945018@qq.com>
2019-11-27 14:19:18 +0800

    新建第一个git项目

---
+++ git-test-project/src/com/jhy/Test.java
@@ -0,0 +1,12 @@
+package com.jhy;
+
+/**
+ * @Date:2019/11/27 14:10
+ * @Author:贾航宇
+ * @注解:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
