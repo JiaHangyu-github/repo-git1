fff2e6955f7c02314fff95b5258b545e3e2d68e1
贾航宇 <1092945018@qq.com>
2019-11-27 14:20:50 +0800

    添加一个方法

--- git-test-project/src/com/jhy/Test.java
+++ git-test-project/src/com/jhy/Test.java
@@ -9,4 +9,10 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("a");
     }
+
+    public void test() {
+        int i = 1;
+        i += 10;
+
+    }
 }
